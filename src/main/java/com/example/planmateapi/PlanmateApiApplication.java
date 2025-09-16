fe2d45068523bee7d1edc27af35f3c8822a23a4f
kietvo5924 <kietcun100@gmail.com>
2025-09-16 16:06:56 +0700

    first commit

---
+++ src/main/java/com/example/planmateapi/PlanmateApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.planmateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanmateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanmateApiApplication.class, args);
+    }
+
+}
